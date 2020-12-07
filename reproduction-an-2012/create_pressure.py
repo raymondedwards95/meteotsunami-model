bc571202ddecb0d41bdbbb65767135e1f95366d8
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2020-12-07 11:59:16 +0100

    update parameters reproduction

--- reproduction-an-2012/create_pressure.py
+++ reproduction-an-2012/create_pressure.py
@@ -2,6 +2,7 @@
 import os
 import sys
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 # fix for importing functions below
@@ -14,7 +15,7 @@ filename = os.path.dirname(os.path.realpath(__file__)) + "\\pressure.amp"
 
 ###
 t0 = 10000.
-U = 50.
+U = - 50.
 a = 200000.
 p0 = 2000.
 
@@ -27,8 +28,9 @@ def pressure(x, y, t, t0=10000, U=50, a=200000, p0=2000):
     )
 
 ###
+print("Computing pressure field")
 x = np.linspace(0, 1e6, 51)
-y = np.linspace(-1e7, 1e7, 75)
+y = np.linspace(-1e7, 1e7, 501)
 t = np.arange(0, 70, 1) * 3600.
 
 p = np.zeros((t.size, y.size, x.size))
@@ -38,5 +40,22 @@ for i in range(t.size):
             p[i,j,k] = pressure(x[k], y[j], t[i])
 
 ###
+print("Writing pressure field")
 data = fp.convert_to_xarray(t, x, y, p)
 fp.write_pressure(data, filename)
+
+###
+print("Plotting pressure field")
+fig, ax = plt.subplots(1, 5, sharey=True)
+fig.set_tight_layout(True)
+im = [None] * 5
+for i in range(5):
+    idx = t.size * i // 5
+    im[i] = ax[i].contourf(x/1000., y/1000., p[idx,:,:], vmin=0, vmax=p0)
+    ax[i].set_title(f"$t = {t[idx] : 0.0f}$s")
+    ax[i].set_xlabel("x [km]")
+    ax[i].set_xlim([0, 500])
+ax[0].set_ylabel("y [km]")
+fig.colorbar(im[-2], ax=ax[-1])
+plt.savefig("./example_pressure.jpg")
+plt.show()
