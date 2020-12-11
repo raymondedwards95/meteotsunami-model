f59e22c3127f7e077f9e5044463ec0da9742e368
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2020-12-11 13:47:18 +0100

    update paths + decrease filesize pressure

--- reproduction-an-2012/create_pressure.py
+++ reproduction-an-2012/create_pressure.py
@@ -10,16 +10,17 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 import functions.pressure as fp
 
 
-###
-filename = os.path.dirname(os.path.realpath(__file__)) + "\\pressure.amp"
+### Filenames
+current_dir = os.path.dirname(os.path.realpath(__file__))
+filename = current_dir + "\\pressure.amp"
 
-###
+### Parameters
 t0 = 10000.
 U = - 50.
 a = 200000.
 p0 = 2000.
 
-###
+### Function
 def pressure(x, y, t, t0=10000, U=50, a=200000, p0=2000):
     return (
         p0
@@ -27,24 +28,32 @@ def pressure(x, y, t, t0=10000, U=50, a=200000, p0=2000):
         * np.exp(-(x**2. + (y - U * t)**2. ) / a**2.)
     )
 
-###
+### Compute field
 print("Computing pressure field")
-x = np.linspace(0, 1e6, 51)
-y = np.linspace(-1e7, 1e7, 501)
-t = np.arange(0, 70, 1) * 3600.
+x = np.linspace(0, 1e6, 75, dtype=np.float)
+y = np.linspace(-1e7, 1e7, 751, dtype=np.float)
+t = np.arange(0, 70, 1, dtype=np.float) * 3600.
 
+# loop over all t, y and x
 p = np.zeros((t.size, y.size, x.size))
 for i in range(t.size):
     for j in range(y.size):
         for k in range(x.size):
             p[i,j,k] = pressure(x[k], y[j], t[i])
 
-###
+# remove zero-columns and zero-rows
+ix = np.where(~ np.all(np.isclose(p, 0), axis=(0,1)))[0]
+iy = np.where(~ np.all(np.isclose(p, 0), axis=(0,2)))[0]
+x = x[ix]
+y = y[iy]
+p = p[:,:,ix][:,iy,:]
+
+### Write field
 print("Writing pressure field")
 data = fp.convert_to_xarray(t, x, y, p)
 fp.write_pressure(data, filename)
 
-###
+### Visualise field
 print("Plotting pressure field")
 fig, ax = plt.subplots(1, 5, sharey=True)
 fig.set_tight_layout(True)
@@ -57,5 +66,5 @@ for i in range(5):
     ax[i].set_xlim([0, 500])
 ax[0].set_ylabel("y [km]")
 fig.colorbar(im[-2], ax=ax[-1])
-plt.savefig("./example_pressure.jpg")
+plt.savefig(current_dir + "\\test_pressure.jpg")
 plt.show()
