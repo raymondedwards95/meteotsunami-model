2f783986aead5ec84f318f8699c7b06821d45482
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2020-12-15 12:14:34 +0100

    add script to run all cases

---
+++ reproduction-an-2012/run_all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Runs and make figures for all cases with name 'input_repr_**.mdu' in this folder
+# make sure the settings in the root folder are correct, following the template
+
+# set path to model executables
+if [ ! -f ../settings.sh ]
+  then
+    echo "../settings.sh does not exist. Creating file... Probably it needs to be adjusted manually."
+    cp ../setup/settings.sh ../settings.sh
+fi
+
+source ../settings.sh
+echo "Path to executables is $DFLOWFM_BIN_PATH"
+
+# check if executable exists
+if [ ! -f $INPUT_FILE ]
+  then
+    echo "$DFLOWFM_BIN_PATH/run_dflowfm.sh does not exist. Check ../setup/settings.sh for errors. Exiting..."
+    exit 1
+fi
+
+# run cases
+bash ./run_reproduction.sh 00 &
+bash ./run_reproduction.sh 01 &
+bash ./run_reproduction.sh 02 &
+
+exit 0
