125ddc59c1826dd847e208a4bd3dafbf584e2b12
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-01-29 11:27:46 +0100

    update pressure-files

--- functions/pressure.py
+++ functions/pressure.py
@@ -100,7 +100,7 @@ unit1           = Pa
                 f"TIME = {t[i]/3600.:0.06f} hours since 1970-01-01 00:00:00 +00:00\n")
 
             # put nothing if all values are close to reference
-            # if np.all(np.isclose(data.values[i, :, :].round(2), 0.)):  # maybe set rtol and atol?
+            # if ((i != 0) and (np.all(np.isclose(data.values[i, :, :].round(2), 0.)))):  # maybe set rtol and atol?
             #     pass
             if False:
                 pass
