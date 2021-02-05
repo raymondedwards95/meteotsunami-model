e9d328e8c3b93bf409e4cd49bf41a189989871b7
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-02-05 16:28:44 +0100

    add observation points and cross sections

--- reproduction-an-2012/create_grid.py
+++ reproduction-an-2012/create_grid.py
@@ -1,13 +1,19 @@
-""" Creates a file that contains a gridfor D3D-FM-FLOW 
+""" Creates a file that contains a grid for D3D-FM-FLOW 
 Note: this script should be run from Delta-Shell
 """
 
 import os
+import sys
 
 import Libraries.FlowFlexibleMeshFunctions as FFMF
 import Libraries.StandardFunctions as SF
 
+# fix for importing functions below
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+import functions.observations as fo
 
+
+## Grid
 x_min = 0
 x_max = 1e6
 x_step = 1e4
@@ -17,6 +23,7 @@ y_max = +1e7
 y_step = 1e4
 
 
+## Write to file
 x_length = x_max - x_min
 x_num = int(x_length / x_step) + 1
 
@@ -28,3 +35,18 @@ SF.AddToProject(model)
 FFMF.GenerateRegularGridForModelUsingExtend(
     model, x_length, y_length, x_num, y_num, x_min, y_min)
 model.WriteNetFile(os.path.dirname(os.path.realpath(__file__)) + "/FlowFM_net.nc")
+
+
+## Observation Points and Cross Sections
+obs = []
+obs.append(fo.ObservationPoint("Test Point", x=10e3, y=x_max/2.))
+
+for i in range(4):
+    _x = i * 1e4
+    obs.append(fo.ObservationCrossSection(name=f"Along Shore x={_x/1000:0.0f}km", x=[_x, _x], y=[y_min, y_max]))
+
+for i in range(5):
+    _y = i*y_max/4
+    obs.append(fo.ObservationCrossSection(name=f"Cross Shore y={_y/1000:0.0f}km", x=[x_min, x_max], y=[_y, _y]))
+
+fo.write_observations(obs, filename=os.path.dirname(os.path.realpath(__file__)) + "/obs_repr_00")
