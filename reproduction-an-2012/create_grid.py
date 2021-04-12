ba6b095a6ca3ff8d516c84c056bc7466a448fef3
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-04-12 14:24:09 +0200

    move grid files to their own folder

--- reproduction-an-2012/create_grid.py
+++ reproduction-an-2012/create_grid.py
@@ -10,6 +10,11 @@ import Libraries.FlowFlexibleMeshFunctions as FFMF
 import Libraries.StandardFunctions as SF
 
 
+## Filename
+grid_dir = os.path.dirname(os.path.realpath(__file__)) + "/grid"
+os.makedirs(grid_dir, exist_ok=True)
+
+
 ## Grid
 case = [0, 20, 21, 22]
 num_cases = len(case)
@@ -34,7 +39,7 @@ for i in range(num_cases):
     x_num = int(x_length / x_step[i]) + 1    
     y_num = int(y_length / y_step[i]) + 1
 
-    net_file_path = os.getcwd() + "/grid_repr_{:02.0f}_net.nc".format(float(case[i]))
+    net_file_path = grid_dir + "/repr_{:02.0f}_net.nc".format(float(case[i]))
 
     model = FFMF.WaterFlowFMModel()
     # FFMF.SetModelProperty(model, "NetFilePath", net_file_path)
@@ -43,3 +48,5 @@ for i in range(num_cases):
     # model.NetFilePath = net_file_path
     FFMF.GenerateRegularGridForModelUsingExtend(model, x_length, y_length, x_num, y_num, x_min, y_min)
     model.WriteNetFile(net_file_path)
+
+    print("Finished writing to", net_file_path)
