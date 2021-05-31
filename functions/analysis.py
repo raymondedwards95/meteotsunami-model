9bc590889402605e8ea15450a6a427b0d4238ebc
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-05-31 11:34:03 +0200

    add test for computation of decay parameter

--- functions/analysis.py
+++ functions/analysis.py
@@ -28,3 +28,28 @@ def compute_decay_parameter(data, y, t):
     k0, y0 = popt
     return k0, y0
 
+
+def _test_compute_decay_parameter(showfig=True):
+    k0 = 1./12.
+    y0 = 0.7
+
+    x = np.arange(100)
+    y = exp_decay(x, k0, y0) + np.random.normal(scale=0.01, size=x.size)
+
+    (k0_est, y0_est), _ = curve_fit(
+        exp_decay,
+        x,
+        y
+    )
+
+    plt.figure()
+    plt.scatter(x, y)
+    plt.plot(x, exp_decay(x, k0_est, y0_est), "C1", label=f"$1/k_0={1/k0_est:0.1f}$; $y_0={y0_est:0.2f}$")
+    plt.legend()
+    plt.xlabel("$x$")
+    plt.ylabel("$y$")
+    plt.show()
+
+
+if __name__ == "__main__":
+    _test_compute_decay_parameter(showfig=True)
