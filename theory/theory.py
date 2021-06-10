a1f668644dba5fdb4f20cdd899548db96ebe4c2e
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-06-10 22:07:48 +0200

    update theory section

--- theory/theory.py
+++ theory/theory.py
@@ -1,12 +1,18 @@
 """ Scripts to make figures for help with explaining theory """
 
+import os
+import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+
 g = 9.81
-a = np.arange(0, 15) * 1e5
-alpha = np.array([1/400, 1/40])
+a = np.logspace(2, 6)
+alpha = np.array([1/400, 1/40, 1/4])
 
 
 def u_crit(a, alpha):
@@ -18,11 +24,14 @@ def wavelength(U, alpha):
 
 plt.figure()
 for i in range(alpha.size):
-    plt.plot(a / 1000., u_crit(a, alpha[i]), label=f"$\\alpha = {alpha[i]}$")
+    plt.semilogx(a / 1000., u_crit(a, alpha[i]), label=f"$\\alpha = {alpha[i]}$")
 plt.legend()
 plt.title("Critical Storm Speed")
 plt.xlabel("$a$ [km]")
 plt.ylabel("$U_{crit}$ [m/s]")
+plt.grid()
+plt.ylim(0, 80)
+plt.savefig(f"{current_dir}/theory_speed_size", bbox_inches="tight")
 
 
 plt.figure()
@@ -33,6 +42,8 @@ plt.legend()
 plt.title("Wavelength of Edge Wave Packet")
 plt.xlabel("$a$ [km]")
 plt.ylabel("$\\lambda$ [km]")
+plt.grid()
+plt.savefig(f"{current_dir}/theory_wavelength_size", bbox_inches="tight")
 
 
 plt.show()
