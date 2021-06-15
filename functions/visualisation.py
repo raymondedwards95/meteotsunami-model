eb819a73e2b1b4998444fb6be748c602e63ca342
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-06-15 11:26:47 +0200

    add
    start of new functions

--- functions/visualisation.py
+++ functions/visualisation.py
@@ -1,2 +1,13 @@
 """ Functions for visualising model outputs """
 
+
+def vis_timeseries(data, x=1e4, y=1e4):
+    raise NotImplementedError
+
+
+def vis_alongshore(data, t=3600, x=None):
+    raise NotImplementedError
+
+
+def vis_crossshore(data, y=1e4, t=None):
+    raise NotImplementedError
