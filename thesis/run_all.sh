7cb5f885402a0ad26ad64221944bd3120bc6507a
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-06-15 12:23:26 +0200

    Update run_all.sh

--- thesis/run_all.sh
+++ thesis/run_all.sh
@@ -22,6 +22,28 @@ fi
 echo "Last runs:" > last_runs.log
 
 # run cases
-bash ./run_exp.sh 00
+bash ./run_exp.sh 00 &
+bash ./run_exp.sh 01 &
+bash ./run_exp.sh 02 &
+bash ./run_exp.sh 03 &
+bash ./run_exp.sh 04 &
+bash ./run_exp.sh 05 &
+wait
+
+bash ./run_exp.sh 06 &
+bash ./run_exp.sh 07 &
+bash ./run_exp.sh 08 &
+bash ./run_exp.sh 09 &
+bash ./run_exp.sh 10 &
+bash ./run_exp.sh 11 &
+wait
+
+bash ./run_exp.sh 12 &
+bash ./run_exp.sh 13 &
+bash ./run_exp.sh 14 &
+bash ./run_exp.sh 15 &
+bash ./run_exp.sh 16 &
+bash ./run_exp.sh 17 &
+wait
 
 exit 0
