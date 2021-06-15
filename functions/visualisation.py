50b6182553d2ff4cbfa7989d517641f60b82afec
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-06-15 11:40:26 +0200

    add exploration
    layout of figure

--- functions/visualisation.py
+++ functions/visualisation.py
@@ -1,8 +1,35 @@
 """ Functions for visualising model outputs """
 
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
 
 def vis_timeseries(data, x=1e4, y=1e5):
-    raise NotImplementedError
+    if isinstance(y, (int, float)):
+        y = [y]
+
+    t = data["t"]
+    wl = data["wl"].interp(x=x, y=y)
+    p = data["p"].interp(x=x, y=y)
+
+    fig, ax = plt.subplots(2, 1, sharex=True)
+    ax[0].plot(t, wl)
+    ax[1].plot(t, p)
+    for i in range(2):
+        ax[i].axhline(color="black", linewidth=1)
+
+    fig, ax = plt.subplots(len(y), 1, sharex=True)
+    for i in range(len(y)):
+        ax[i].plot(t, wl[:, i])
+        ax[i].plot(t, p[:, i] / 2000.)
+        ax[i].axhline(color="black", linewidth=1)
+
+    fig, ax = plt.subplots(2, len(y), sharex=True, sharey=True)
+    for i in range(len(y)):
+        ax[0,i].plot(t, wl[:, i])
+        ax[1,i].plot(t, p[:, i] / 2000.)
+        for j in range(2):
+            ax[j,i].axhline(color="black", linewidth=1)
 
 
 def vis_alongshore(data, t=3600, x=1e4):
