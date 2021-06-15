353932ce7cd6f08de29140cb545974f731c92a82
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-06-15 11:27:57 +0200

    adjust

--- functions/visualisation.py
+++ functions/visualisation.py
@@ -1,13 +1,13 @@
 """ Functions for visualising model outputs """
 
 
-def vis_timeseries(data, x=1e4, y=1e4):
+def vis_timeseries(data, x=1e4, y=1e5):
     raise NotImplementedError
 
 
-def vis_alongshore(data, t=3600, x=None):
+def vis_alongshore(data, t=3600, x=1e4):
     raise NotImplementedError
 
 
-def vis_crossshore(data, y=1e4, t=None):
+def vis_crossshore(data, y=1e5, t=3600):
     raise NotImplementedError
