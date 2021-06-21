d648d03b06c51ff31c6472524b8a9f2cf876ad0d
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-06-21 16:11:48 +0200

    adjust script

--- thesis/create_all.sh
+++ thesis/create_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Run all scripts for creating necessary files
 
-python3 ./create_bathymetry.py &
-python3 ./create_observations.py &
-python3 ./create_pressure.py &
+python3 ./create_bathymetry.py
+python3 ./create_observations.py
+python3 ./create_pressure.py
 
 wait
 exit 0
