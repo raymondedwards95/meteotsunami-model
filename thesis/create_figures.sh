2ae3cf9f003b06292dbd2321aa1adff0cd0410d3
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-06-22 17:22:59 +0200

    fix

--- thesis/create_figures.sh
+++ thesis/create_figures.sh
@@ -7,7 +7,7 @@ python3 ./create_visualisation.py 03 &
 python3 ./create_visualisation.py 04 &
 python3 ./create_visualisation.py 05 &
 wait
-echo "Created figures exp: 00 01 02 03 04 05" >> last_runs.log
+echo "Created figures exp: 00 01 02 03 04 05\n" >> last_runs.log
 
 python3 ./create_visualisation.py 06 &
 python3 ./create_visualisation.py 07 &
@@ -16,7 +16,7 @@ python3 ./create_visualisation.py 09 &
 python3 ./create_visualisation.py 10 &
 python3 ./create_visualisation.py 11 &
 wait
-echo "Created figures exp: 06 07 08 09 10 11" >> last_runs.log
+echo "Created figures exp: 06 07 08 09 10 11\n" >> last_runs.log
 
 python3 ./create_visualisation.py 12 &
 python3 ./create_visualisation.py 13 &
@@ -25,6 +25,6 @@ python3 ./create_visualisation.py 15 &
 python3 ./create_visualisation.py 16 &
 python3 ./create_visualisation.py 17 &
 wait
-echo "Created figures exp: 12 13 14 15 16 17" >> last_runs.log
+echo "Created figures exp: 12 13 14 15 16 17\n" >> last_runs.log
 
 exit 0
