6a3b8abe6875a21e6b6f81307c1014e4123d3167
Raymond Edwards <raymond.edwards@knmi.nl>
2021-06-30 19:50:33 +0000

    fix

--- reproduction-an-2012/create_figures.sh
+++ reproduction-an-2012/create_figures.sh
@@ -1,38 +1,38 @@
 
 # run cases
-python3 ./create_visualisation.py 00 &
-python3 ./create_visualisation.py 01 &
-python3 ./create_visualisation.py 02 &
-python3 ./create_visualisation.py 03 &
-python3 ./create_visualisation.py 04 &
-python3 ./create_visualisation.py 05 &
+python3 ./create_visualisations.py 00 &
+python3 ./create_visualisations.py 01 &
+python3 ./create_visualisations.py 02 &
+python3 ./create_visualisations.py 03 &
+python3 ./create_visualisations.py 04 &
+python3 ./create_visualisations.py 05 &
 wait
 echo "Created figures exp: 00 01 02 03 04 05\n" >> last_runs.log
 
-python3 ./create_visualisation.py 10 &
-python3 ./create_visualisation.py 11 &
-python3 ./create_visualisation.py 12 &
-python3 ./create_visualisation.py 15 &
-python3 ./create_visualisation.py 16 &
-python3 ./create_visualisation.py 17 &
+python3 ./create_visualisations.py 10 &
+python3 ./create_visualisations.py 11 &
+python3 ./create_visualisations.py 12 &
+python3 ./create_visualisations.py 15 &
+python3 ./create_visualisations.py 16 &
+python3 ./create_visualisations.py 17 &
 wait
 echo "Created figures exp: 10 11 12 15 16 17\n" >> last_runs.log
 
-python3 ./create_visualisation.py 20 &
-python3 ./create_visualisation.py 21 &
-python3 ./create_visualisation.py 31 &
-python3 ./create_visualisation.py 32 &
-python3 ./create_visualisation.py 33 &
-python3 ./create_visualisation.py 36 &
+python3 ./create_visualisations.py 20 &
+python3 ./create_visualisations.py 21 &
+python3 ./create_visualisations.py 31 &
+python3 ./create_visualisations.py 32 &
+python3 ./create_visualisations.py 33 &
+python3 ./create_visualisations.py 36 &
 wait
 echo "Created figures exp: 20 21 31 32 33 36\n" >> last_runs.log
 
-python3 ./create_visualisation.py 37 &
-python3 ./create_visualisation.py 38 &
-python3 ./create_visualisation.py 39 &
-python3 ./create_visualisation.py 41 &
-python3 ./create_visualisation.py 42 &
-python3 ./create_visualisation.py 43 &
+python3 ./create_visualisations.py 37 &
+python3 ./create_visualisations.py 38 &
+python3 ./create_visualisations.py 39 &
+python3 ./create_visualisations.py 41 &
+python3 ./create_visualisations.py 42 &
+python3 ./create_visualisations.py 43 &
 wait
 echo "Created figures exp: 37 38 39 41 42 43\n" >> last_runs.log
 
