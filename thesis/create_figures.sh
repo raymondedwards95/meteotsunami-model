fc17db1659da59826b70c19b83c786a69613741c
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-07-02 10:57:21 +0200

    fix

--- thesis/create_figures.sh
+++ thesis/create_figures.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
+# Create visualisations for all experiments
 
-# run cases
 python3 ./create_visualisations.py 00 &
 python3 ./create_visualisations.py 01 &
 python3 ./create_visualisations.py 02 &
