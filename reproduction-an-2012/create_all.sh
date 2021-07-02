6f6c27b0f4f5816f867a01d5c0096ca980c0ee15
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-07-02 11:43:40 +0200

    update

--- reproduction-an-2012/create_all.sh
+++ reproduction-an-2012/create_all.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # Run all scripts for creating necessary files
 
-python3 ./create_bathymetry.py &
-python3 ./create_observations.py &
-python3 ./create_pressure.py &
+echo "$(date) - Creating additional input files" >> last_runs.log
 
+# Run scripts
+python3 ./create_bathymetry.py
+python3 ./create_observations.py
+python3 ./create_pressure.py
 wait
+echo "$(date) - Finished creating additional input files" >> last_runs.log
+
 exit 0
