fed93ce82e7dbab0c08484dacbffb3f2c05cef25
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-07-02 12:47:40 +0200

    update grid creation script

--- thesis/create_grid.py
+++ thesis/create_grid.py
@@ -10,7 +10,7 @@ import Libraries.FlowFlexibleMeshFunctions as FFMF
 import Libraries.StandardFunctions as SF
 
 
-## Filename
+## Filename (adjust following!)
 grid_dir = os.path.dirname(os.path.realpath(__file__)) + "/grid"
 os.makedirs(grid_dir, exist_ok=True)
 
@@ -39,11 +39,11 @@ for i in range(num_cases):
     net_file_path = grid_dir + "/exp_{:02.0f}_net.nc".format(float(case[i]))
 
     model = FFMF.WaterFlowFMModel()
-    # FFMF.SetModelProperty(model, "NetFilePath", net_file_path)
     SF.AddToProject(model)
     model.WriteNetFile(net_file_path)
-    # model.NetFilePath = net_file_path
     FFMF.GenerateRegularGridForModelUsingExtend(model, x_length, y_length, x_num, y_num, x_min, y_min)
     model.WriteNetFile(net_file_path)
 
     print("Finished writing to", net_file_path)
+
+print("Finished writing grid")
