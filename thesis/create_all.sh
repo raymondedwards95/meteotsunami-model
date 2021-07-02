b2b58bef1bee3f59548806cacd984e36d7d60266
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-07-02 11:41:09 +0200

    update

--- thesis/create_all.sh
+++ thesis/create_all.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # Run all scripts for creating necessary files
 
+echo "$(date) - Creating additional input files" >> last_runs.log
+
 python3 ./create_bathymetry.py
 python3 ./create_observations.py
 python3 ./create_pressure.py
 
 wait
+echo "$(date) - Finished creating additional input files" >> last_runs.log
+
 exit 0
