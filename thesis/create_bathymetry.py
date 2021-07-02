547f7497cb275fb8625317cd84b2b246d563f6bf
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-07-02 12:32:14 +0200

    update

--- thesis/create_bathymetry.py
+++ thesis/create_bathymetry.py
@@ -11,30 +11,29 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 import functions.bathymetry as fb
 
 
-###
-@np.vectorize
+### Function
 def exponential_shelf(x, h=20, a=1e-5):
     return -1. * h * (1. - np.exp(- 1. * a * x))
 
 
-###
+### Settings
 bathymetry_dir = os.path.dirname(os.path.realpath(__file__)) + "/bathymetry"
 os.makedirs(bathymetry_dir, exist_ok=True)
 
 
-###
+### Compute bathymetry
 x = np.concatenate(([0], np.logspace(0, 6, 51)))
 y = np.linspace(-2e6, +3e6, 5)
 xx, yy = np.meshgrid(x, y)
 zz = exponential_shelf(xx, a=5e-4)
 
 
-###
+### Write to file
 data = fb.convert_to_xarray(x, y, zz)
 fb.write_bathymetry(data, f"{bathymetry_dir}/exp_00.xyb")
 
 
-###
+### Visualize
 fb.plot_bathymetry(data, f"{bathymetry_dir}/fig_exp_00", xmax=10e3)
 
 # plt.show()
