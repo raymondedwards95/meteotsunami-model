d124b44824b7e59b41c45557cc41573222c13b0d
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-07-02 11:19:48 +0200

    remove cartopy as requirement

--- theory/theory_part1.py
+++ theory/theory_part1.py
@@ -3,12 +3,16 @@
 import os
 import sys
 
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
+try:
+    import cartopy.crs as ccrs
+    import cartopy.feature as cfeature
+    create_map = True
+except:
+    create_map = False
 
 sns.set_palette(sns.color_palette("muted"))
 current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -50,21 +54,22 @@ plt.grid()
 plt.savefig(f"{current_dir}/line_wavelength_size", bbox_inches="tight")
 
 
-fig = plt.figure()
-ax = fig.add_subplot(projection=ccrs.PlateCarree())
-ax.coastlines("50m")
-ax.add_feature(
-    cfeature.NaturalEarthFeature(
-        "physical", "land", "50m",
-        edgecolor="black", facecolor="gray"
+if create_map:
+    fig = plt.figure()
+    ax = fig.add_subplot(projection=ccrs.PlateCarree())
+    ax.coastlines("50m")
+    ax.add_feature(
+        cfeature.NaturalEarthFeature(
+            "physical", "land", "50m",
+            edgecolor="black", facecolor="gray"
+        )
     )
-)
-ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True)
-ax.set_xlim([-10, 10])
-ax.set_ylim([45, 60])
-# plt.xlabel("Longitude")
-# plt.ylabel("Latitude")
-plt.savefig(f"{current_dir}/map", bbox_inches="tight")
+    ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True)
+    ax.set_xlim([-10, 10])
+    ax.set_ylim([45, 60])
+    # plt.xlabel("Longitude")
+    # plt.ylabel("Latitude")
+    plt.savefig(f"{current_dir}/map", bbox_inches="tight")
 
 
 plt.show()
