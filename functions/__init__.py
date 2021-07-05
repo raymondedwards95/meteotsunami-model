e42e0ed5ab14f2edd2d477b80d7e5af9e29ed7a9
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-07-05 11:34:43 +0200

    add settings for figures

---
+++ functions/__init__.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+
+### Colors
+sns.set_palette(sns.color_palette("muted"))
+
+### Figure size
+FIGSIZE_NORMAL = (6, 4)
+FIGSIZE_WIDE = (6, 3)
+FIGSIZE_SMALL = (3, 2)
+
+FIG_DPI = 300
