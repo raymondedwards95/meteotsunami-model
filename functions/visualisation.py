7d33f2437c80f3f3878395581a8e28535d729d41
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-07-14 22:46:23 +0200

    update

--- functions/visualisation.py
+++ functions/visualisation.py
@@ -125,6 +125,8 @@ def vis_timeseries(data, y, x=1e4, t_max=None, saveloc=None, keep_open=False):
         ax[i].tick_params(axis="y", labelcolor=f"C0")
         ax2[i].tick_params(axis="y", labelcolor=f"C1")
 
+        ax[i].grid()
+        
 
     ax[-1].set_ylabel("Water Level [m]")
     ax2[-1].set_ylabel("Surface Pressure [Pa]")
