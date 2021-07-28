2112f1db02bc085bc77e7bcbfdbbf501c02f39ab
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-07-28 14:14:56 +0200

    add new common figsize

--- functions/__init__.py
+++ functions/__init__.py
@@ -8,6 +8,7 @@ sns.set_palette(sns.color_palette("muted"))
 ### Figure size
 FIGSIZE_NORMAL = (7, 4)
 FIGSIZE_LONG = (7, 9)
+FIGSIZE_HIGH = (7, 6)
 FIGSIZE_WIDE = (7, 3)
 FIGSIZE_SMALL = (3, 2)
 FIGSIZE_SQUARE = (7, 7)
