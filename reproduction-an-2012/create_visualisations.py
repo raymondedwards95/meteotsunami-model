09872f71f250cf16cfe489bc95720d453736e41b
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-07-29 12:22:45 +0200

    add error handling

--- reproduction-an-2012/create_visualisations.py
+++ reproduction-an-2012/create_visualisations.py
@@ -110,57 +110,63 @@ p = data["p"]
 x = x - x.min()
 
 
-print(f"Creating figures in \n'{figure_dir}'")
-### Figure bathymetry
-print("Figure: bathymetry")
-plt.figure(figsize=FIGSIZE_NORMAL, dpi=FIG_DPI)
+try:
+    print(f"Creating figures in \n'{figure_dir}'")
+    ### Figure bathymetry
+    print("Figure: bathymetry")
+    plt.figure(figsize=FIGSIZE_NORMAL, dpi=FIG_DPI)
 
-plt.contourf(x/1000, y/1000, b)
-plt.colorbar()
+    plt.contourf(x/1000, y/1000, b)
+    plt.colorbar()
 
-plt.title("Water depth [m]")
-plt.xlabel("$x$ [km]")
-plt.ylabel("$y$ [km]")
+    plt.title("Water depth [m]")
+    plt.xlabel("$x$ [km]")
+    plt.ylabel("$y$ [km]")
 
-plt.axhline(color="black", linewidth=1)
-plt.axvline(color="black", linewidth=1)
+    plt.axhline(color="black", linewidth=1)
+    plt.axvline(color="black", linewidth=1)
 
-# plt.axhline(7.56e3, linestyle="--", color="gray", linewidth=1)
-# plt.axvline(10, linestyle="--", color="gray", linewidth=1)
-# plt.axvline(100, linestyle="--", color="gray", linewidth=1)
-# plt.axvline(200, linestyle="--", color="gray", linewidth=1)
-    
-plt.savefig(figure_dir + "bathymetry_contour", bbox_inches="tight", dpi=FIG_DPI)
+    # plt.axhline(7.56e3, linestyle="--", color="gray", linewidth=1)
+    # plt.axvline(10, linestyle="--", color="gray", linewidth=1)
+    # plt.axvline(100, linestyle="--", color="gray", linewidth=1)
+    # plt.axvline(200, linestyle="--", color="gray", linewidth=1)
+        
+    plt.savefig(figure_dir + "bathymetry_contour", bbox_inches="tight", dpi=FIG_DPI)
+except:
+    print(f"Error in bottom-profile visualisation {case=}")
 
 
 ### Figure pressure
 print("Figure: pressure")
 plot_times = [4e4, 8e4, 12e4, 16e4]
 
-fig, ax = plt.subplots(2, 2, sharex=True, sharey=True)
-fig.set_size_inches(FIGSIZE_SQUARE)
-fig.set_dpi(FIG_DPI)
-fig.suptitle("Pressure distribution [Pa]")
-fig.set_tight_layout(True)
+try:
+    fig, ax = plt.subplots(2, 2, sharex=True, sharey=True)
+    fig.set_size_inches(FIGSIZE_SQUARE)
+    fig.set_dpi(FIG_DPI)
+    fig.suptitle("Pressure distribution [Pa]")
+    fig.set_tight_layout(True)
 
-for i in range(4):
-    _ax = ax[i//2, i%2]
-    im = _ax.contourf(x/1000, y/1000, p.interp(t=fu.to_timestr(plot_times[i])), vmin=0, vmax=2000)
-    _ax.set_xlim([0, 300])
-    _ax.set_ylim([0, y.max()/1000])
-    _ax.set_title(f"$t = {plot_times[i] : 0.0f}$s")
-    
-    if i//2: _ax.set_xlabel("$x$ [km]")
-    if not i%2: _ax.set_ylabel("$y$ [km]")
-    
-    fig.colorbar(im, ax=_ax)
-    
-#     _ax.axhline(7.56e3, linestyle="--", color="gray", linewidth=1)
-#     _ax.axvline(10, linestyle="--", color="gray", linewidth=1)
-#     _ax.axvline(100, linestyle="--", color="gray", linewidth=1)
-#     _ax.axvline(200, linestyle="--", color="gray", linewidth=1)
-    
-fig.savefig(figure_dir + "pressure_contours", bbox_inches="tight", dpi=FIG_DPI)
+    for i in range(4):
+        _ax = ax[i//2, i%2]
+        im = _ax.contourf(x/1000, y/1000, p.interp(t=fu.to_timestr(plot_times[i])), vmin=0, vmax=2000)
+        _ax.set_xlim([0, 300])
+        _ax.set_ylim([0, y.max()/1000])
+        _ax.set_title(f"$t = {plot_times[i] : 0.0f}$s")
+        
+        if i//2: _ax.set_xlabel("$x$ [km]")
+        if not i%2: _ax.set_ylabel("$y$ [km]")
+        
+        fig.colorbar(im, ax=_ax)
+        
+    #     _ax.axhline(7.56e3, linestyle="--", color="gray", linewidth=1)
+    #     _ax.axvline(10, linestyle="--", color="gray", linewidth=1)
+    #     _ax.axvline(100, linestyle="--", color="gray", linewidth=1)
+    #     _ax.axvline(200, linestyle="--", color="gray", linewidth=1)
+        
+    fig.savefig(figure_dir + "pressure_contours", bbox_inches="tight", dpi=FIG_DPI)
+except:
+    print(f"Error in pressure visualisation {case=}")
 
 
 ### Figure sse contours
@@ -168,29 +174,31 @@ print("Figure: sse contours")
 # figure 1
 plot_times = [4e4, 8e4, 12e4, 16e4]
 
-fig, ax = plt.subplots(2, 2, sharex=True, sharey=True)
-fig.set_size_inches(FIGSIZE_SQUARE)
-fig.set_dpi(FIG_DPI)
-fig.suptitle("Sea Surface Elevation [m]")
-for i in range(4):
-    _ax = ax[i//2, i%2]
-    im = _ax.contourf(x/1000, y/1000, wl.interp(t=fu.to_timestr(plot_times[i])), vmin=-0.8, vmax=0.8)
-    _ax.set_xlim([0, 300])
-    _ax.set_ylim([0, y.max()/1000])
-    _ax.set_title(f"$t = {plot_times[i] : 0.0f}$s")
-    
-    if i//2: _ax.set_xlabel("$x$ [km]")
-    if not i%2: _ax.set_ylabel("$y$ [km]")
-    
-    fig.colorbar(im, ax=_ax)
-    
-#     _ax.axhline(7.56e3, linestyle="--", color="gray", linewidth=1)
-#     _ax.axvline(10, linestyle="--", color="gray", linewidth=1)
-#     _ax.axvline(100, linestyle="--", color="gray", linewidth=1)
-#     _ax.axvline(200, linestyle="--", color="gray", linewidth=1)
-    
-fig.savefig(figure_dir + "sse_contours", bbox_inches="tight", dpi=FIG_DPI)
-
+try:
+    fig, ax = plt.subplots(2, 2, sharex=True, sharey=True)
+    fig.set_size_inches(FIGSIZE_SQUARE)
+    fig.set_dpi(FIG_DPI)
+    fig.suptitle("Sea Surface Elevation [m]")
+    for i in range(4):
+        _ax = ax[i//2, i%2]
+        im = _ax.contourf(x/1000, y/1000, wl.interp(t=fu.to_timestr(plot_times[i])), vmin=-0.8, vmax=0.8)
+        _ax.set_xlim([0, 300])
+        _ax.set_ylim([0, y.max()/1000])
+        _ax.set_title(f"$t = {plot_times[i] : 0.0f}$s")
+        
+        if i//2: _ax.set_xlabel("$x$ [km]")
+        if not i%2: _ax.set_ylabel("$y$ [km]")
+        
+        fig.colorbar(im, ax=_ax)
+        
+    #     _ax.axhline(7.56e3, linestyle="--", color="gray", linewidth=1)
+    #     _ax.axvline(10, linestyle="--", color="gray", linewidth=1)
+    #     _ax.axvline(100, linestyle="--", color="gray", linewidth=1)
+    #     _ax.axvline(200, linestyle="--", color="gray", linewidth=1)
+        
+    fig.savefig(figure_dir + "sse_contours", bbox_inches="tight", dpi=FIG_DPI)
+except:
+    print(f"Error in contours visualisation {case=}")
 
 ### Figure alongshore profiles sse
 # figure 2
@@ -198,25 +206,28 @@ print("Figure: sse alongshore profile")
 plot_times = [4e4, 8e4, 12e4, 16e4]
 plot_ylims = np.array([[0, 4], [1, 6], [2, 8], [3, 10]]) * 1e3
 
-fig, ax = plt.subplots(2, 2, sharex=False, sharey=False)
-fig.set_size_inches(FIGSIZE_SQUARE)
-fig.set_dpi(FIG_DPI)
-fig.suptitle("Along-shore profile of Sea Surface Elevation [m]")
-for i in range(4):
-    _ax = ax[i//2, i%2]
-    im = _ax.plot(y/1000, wl.interp(t=fu.to_timestr(plot_times[i]), x=10e3))
-    _ax.set_xlim(plot_ylims[i])
-    _ax.set_ylim([-1, 1])
-    _ax.set_title(f"$t = {plot_times[i] : 0.0f}$s")
-    
-    if i//2: _ax.set_xlabel("$y$ [km]")
-    if not i%2: _ax.set_ylabel("$SSE$ [m]")
+try:
+    fig, ax = plt.subplots(2, 2, sharex=False, sharey=False)
+    fig.set_size_inches(FIGSIZE_SQUARE)
+    fig.set_dpi(FIG_DPI)
+    fig.suptitle("Along-shore profile of Sea Surface Elevation [m]")
+    for i in range(4):
+        _ax = ax[i//2, i%2]
+        im = _ax.plot(y/1000, wl.interp(t=fu.to_timestr(plot_times[i]), x=10e3))
+        _ax.set_xlim(plot_ylims[i])
+        _ax.set_ylim([-1, 1])
+        _ax.set_title(f"$t = {plot_times[i] : 0.0f}$s")
         
-    _ax.axvline(7.56e3, linestyle="--", color="gray", linewidth=1)
-    _ax.axhline(color="black", linewidth=1)
-    _ax.axvline(color="black", linewidth=1)
-    
-fig.savefig(figure_dir + "sse_along", bbox_inches="tight", dpi=FIG_DPI)
+        if i//2: _ax.set_xlabel("$y$ [km]")
+        if not i%2: _ax.set_ylabel("$SSE$ [m]")
+            
+        _ax.axvline(7.56e3, linestyle="--", color="gray", linewidth=1)
+        _ax.axhline(color="black", linewidth=1)
+        _ax.axvline(color="black", linewidth=1)
+        
+    fig.savefig(figure_dir + "sse_along", bbox_inches="tight", dpi=FIG_DPI)
+except:
+    print(f"Error in alongshore-profile visualisation {case=}")
 
 
 ### Figure cross-shore profile sse
@@ -224,23 +235,26 @@ fig.savefig(figure_dir + "sse_along", bbox_inches="tight", dpi=FIG_DPI)
 print("Figure: sse cross-shore profile")
 y_slices = np.array([7.56]) * 1e6
 
-plt.figure(figsize=FIGSIZE_NORMAL, dpi=FIG_DPI)
-for i in range(len(y_slices)):
-    plt.plot(x/1000, wl.interp(t=fu.to_timestr(1.6e5), y=y_slices[i]), label=f"$y={y_slices[i]/1000 : 0.0f}$ km")
-plt.legend()
-plt.axhline(color="black", linewidth=1)
-plt.axvline(color="black", linewidth=1)
-plt.title("Cross-shore profile of Sea Surface Elevation [m]")
-plt.xlabel("$x$ [km]")
-plt.ylabel("$SSE$ [m]")
-plt.xlim([0, 600])
-plt.ylim([0, 0.7])
+try:
+    plt.figure(figsize=FIGSIZE_NORMAL, dpi=FIG_DPI)
+    for i in range(len(y_slices)):
+        plt.plot(x/1000, wl.interp(t=fu.to_timestr(1.6e5), y=y_slices[i]), label=f"$y={y_slices[i]/1000 : 0.0f}$ km")
+    plt.legend()
+    plt.axhline(color="black", linewidth=1)
+    plt.axvline(color="black", linewidth=1)
+    plt.title("Cross-shore profile of Sea Surface Elevation [m]")
+    plt.xlabel("$x$ [km]")
+    plt.ylabel("$SSE$ [m]")
+    plt.xlim([0, 600])
+    plt.ylim([0, 0.7])
 
-# plt.axvline(10, linestyle="--", color="gray", linewidth=1)
-# plt.axvline(100, linestyle="--", color="gray", linewidth=1)
-# plt.axvline(200, linestyle="--", color="gray", linewidth=1)
-    
-plt.savefig(figure_dir + "sse_cross", bbox_inches="tight", dpi=FIG_DPI)
+    # plt.axvline(10, linestyle="--", color="gray", linewidth=1)
+    # plt.axvline(100, linestyle="--", color="gray", linewidth=1)
+    # plt.axvline(200, linestyle="--", color="gray", linewidth=1)
+        
+    plt.savefig(figure_dir + "sse_cross", bbox_inches="tight", dpi=FIG_DPI)
+except:
+    print(f"Error in cross-shore-profile visualisation {case=}")
 
 
 ### Figure alongshore profiles
@@ -249,24 +263,27 @@ print("Figure: sse more alongshore profiles")
 x_slices = np.array([10, 100, 200]) * 1e3
 linestyles = ["-", "--", "--"]
 
-plt.figure(figsize=FIGSIZE_NORMAL, dpi=FIG_DPI)
-plt.title("Along-shore profile of Sea Surface Elevation [m]")
-for i in range(len(x_slices)):
-    plt.plot(
-        y/1000, 
-        wl.interp(t=fu.to_timestr(1.6e5), x=x_slices[i]), 
-        label=f"$x={x_slices[i]/1000 : 0.0f}$ km",
-        linestyle=linestyles[i]
-    )
-plt.legend()
-plt.axhline(color="black", linewidth=1)
-plt.axvline(color="black", linewidth=1)
-plt.xlim([0, y.max()/1000])
-plt.axvline(7.56e3, linestyle="--", color="gray", linewidth=1)
-plt.xlabel("$y$ [km]")
-plt.ylabel("$SSE$ [m]")
-    
-plt.savefig(figure_dir + "sse_along_2", bbox_inches="tight", dpi=FIG_DPI)
+try:
+    plt.figure(figsize=FIGSIZE_NORMAL, dpi=FIG_DPI)
+    plt.title("Along-shore profile of Sea Surface Elevation [m]")
+    for i in range(len(x_slices)):
+        plt.plot(
+            y/1000, 
+            wl.interp(t=fu.to_timestr(1.6e5), x=x_slices[i]), 
+            label=f"$x={x_slices[i]/1000 : 0.0f}$ km",
+            linestyle=linestyles[i]
+        )
+    plt.legend()
+    plt.axhline(color="black", linewidth=1)
+    plt.axvline(color="black", linewidth=1)
+    plt.xlim([0, y.max()/1000])
+    plt.axvline(7.56e3, linestyle="--", color="gray", linewidth=1)
+    plt.xlabel("$y$ [km]")
+    plt.ylabel("$SSE$ [m]")
+        
+    plt.savefig(figure_dir + "sse_along_2", bbox_inches="tight", dpi=FIG_DPI)
+except:
+    print(f"Error in alongshore-profile 2 visualisation {case=}")
 
 
 ### Figures - Contour
