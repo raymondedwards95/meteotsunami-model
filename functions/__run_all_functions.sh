8b1032d977df5e8701c28682fad2b39d245ef69c
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2021-07-29 14:07:11 +0200

    update script

--- functions/__run_all_functions.sh
+++ functions/__run_all_functions.sh
@@ -3,14 +3,29 @@
 
 echo "Running all python-files in this directory $PWD"
 
+echo ">> analysis.py"
 python3 ./analysis.py
-python3 ./animation.py
-python3 ./bathymetry.py
-python3 ./observations.py
-python3 ./pressure.py
-python3 ./regrid.py
-python3 ./utilities.py
-python3 ./visualisation.py
-wait
 
+echo ">> animation.py"
+python3 ./animation.py
+
+echo ">> bathymetry.py"
+python3 ./bathymetry.py
+
+echo ">> observations.py"
+python3 ./observations.py
+
+echo ">> pressure.py"
+python3 ./pressure.py
+
+echo ">> regrid.py"
+python3 ./regrid.py
+
+echo ">> utilities.py"
+python3 ./utilities.py
+
+echo ">> visualisation.py"
+python3 ./visualisation.py
+
+wait
 exit 0
