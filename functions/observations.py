d1121d1c11c036fa2ae2f6cc28cd42a69b8dfdb3
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2022-08-15 21:53:42 +0200

    add function to visualise observation points and lines

--- functions/observations.py
+++ functions/observations.py
@@ -3,6 +3,8 @@
 import os
 import sys
 
+import matplotlib.axes as mpa
+import matplotlib.pyplot as plt
 import numpy.typing as npt
 
 # fix for importing functions below
@@ -36,6 +38,15 @@ class ObservationPoint():
     def __str__(self):
         return f"Observation point '{self.name}'' at x={self.x:.0e} and y={self.y:.0e}".replace("e+00", "")
 
+    def plot(self, ax: mpa.Axes) -> mpa.Axes:
+        """ Plot point on an existing Axes object
+
+        Input:
+            ax:     existing Axes object
+        """
+        ax.plot(self.x, self.y, "o", markersize=5, label=self.name)
+        return ax
+
 
 class ObservationCrossSection():
     """ Lines for observation consisting of two or more points
@@ -68,9 +79,19 @@ class ObservationCrossSection():
     def __str__(self):
         return f"Observation cross-section '{self.name}' between points p_start=({self.x[0]:.0e}, {self.y[0]:.0e}) and p_end=({self.x[-1]:.0e}, {self.y[-1]:.0e})".replace("e+00", "")
 
+    def plot(self, ax: mpa.Axes) -> mpa.Axes:
+        """ Plot lines on an existing Axes object
+
+        Input:
+            ax:     existing Axes object
+        """
+        ax.plot(self.x, self.y, "-o", markersize=5, label=self.name)
+        return ax
+
 
 def write_observations(data: npt.ArrayLike, filename: str) -> None:
     """ Write observation points and observation cross sections to files
+
     Input:
         data:       list of observation points and observation cross sections
         filename:   name of file to write (extensions are added automatically)
@@ -127,14 +148,62 @@ def write_observations(data: npt.ArrayLike, filename: str) -> None:
     return
 
 
+def plot_observations(data: npt.ArrayLike, savename: str=None, keep_open: bool=False) -> plt.Figure:
+    """ Visualise observation points and lines
+
+    Input:
+        data:       list of observation points and observation cross sections
+
+    Options:
+        savename:   name of figure
+        keep_open:  keep figures open after finishing
+    """
+    ## Filename
+    if not savename.endswith(".png"):
+        savename += "_plot.png"
+
+    ## Figure
+    fig, ax = plt.subplots(1, 1)
+    fig.set_size_inches(FIGSIZE_NORMAL)
+    fig.set_dpi(FIG_DPI)
+    fig.set_tight_layout(True)
+
+    ## Layout
+    ax.set_title("Observation Points and Lines")
+    ax.axhline(color="black", linewidth=1)
+    ax.axvline(color="black", linewidth=1)
+
+    ## Points and lines
+    for element in data:
+        ax = element.plot(ax)
+
+    ## Layout part 2
+    ax.grid()
+    ax.legend(loc="upper left", bbox_to_anchor=(1, 1))
+    ax.set_xlabel("$x$ [m]")
+    ax.set_ylabel("$y$ [m]")
+
+    ## End
+    fig.savefig(savename, bbox_inches="tight", dpi=FIG_DPI, pil_kwargs=FIG_PIL_KWARGS)
+    print(f"Saved figure {savename}")
+    if not keep_open:
+        plt.close("all")
+
+    return fig
+
+
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.realpath(__file__))
     obs_dir = f"{script_dir}/tests/obs"
 
     obs_0 = ObservationPoint(name="Center", x=0, y=0)
     obs_1 = ObservationPoint(name="Point", x=1, y=1)
-    obs_2 = ObservationCrossSection(name="Line", x=[-2, 2], y=[0, 0])
-    obs_3 = ObservationCrossSection(name="Curve", x=[-2, 0, 0], y=[0, 0, 2])
-    obs_4 = ObservationCrossSection(name="Square", x=[-4, -4 ,4, 4], y=[-4, 4, 4, -4])
+    obs_2 = ObservationCrossSection(name="Line", x=[-3, 3], y=[3, 3])
+    obs_3 = ObservationCrossSection(name="Diagonal", x=[-2, 3], y=[-4, 1])
+    obs_4 = ObservationCrossSection(name="Curve", x=[-2, 0, -1], y=[-3, 1, 2])
+    obs_5 = ObservationCrossSection(name="Square", x=[-4, -4 ,4, 4], y=[-4, 4, 4, -4])
 
-    write_observations([obs_0, obs_1, obs_2, obs_3, obs_4], filename=obs_dir)
+    data = [obs_0, obs_1, obs_2, obs_3, obs_4, obs_5]
+
+    write_observations(data, filename=obs_dir)
+    plot_observations(data, savename=obs_dir)
