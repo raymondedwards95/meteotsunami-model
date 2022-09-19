a68f053e498278f3285152fdee5e137f9d702955
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2022-09-19 17:25:31 +0200

    prepare new file

---
+++ functions/plotting/profiles.py
@@ -0,0 +1,27 @@
+""" Functions for visualising wave profiles from model output
+
+Main Functions:
+"""
+
+import os
+import sys
+
+import cmocean as cmo
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
+
+# fmt: off
+# fix for importing functions below
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+from functions import *
+import functions.analysis as fa
+# fmt: on
+
+
+class plot_alongshore():
+    raise NotImplementedError
+
+
+class plot_crossshore():
+    raise NotImplementedError
