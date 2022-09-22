da6c06e85d6693a1e2268126c5c4e05c87cd9356
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2022-09-22 14:37:35 +0200

    update function list

--- functions/plotting/__init__.py
+++ functions/plotting/__init__.py
@@ -6,5 +6,8 @@ import sys
 # fix for importing functions below
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 from functions import *
-from functions.plotting.spectrum import plot_spectrum_1d
+from functions.plotting.contour import plot_contour
+from functions.plotting.spectrum import plot_spectrum_1d, plot_spectrum_2d
+from functions.plotting.timeseries import plot_timeseries
+from functions.plotting.profiles import plot_alongshore, plot_crossshore
 # fmt: on
