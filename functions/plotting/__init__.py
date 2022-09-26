8137829207b32802fbacd824f39e334f81bd2495
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2022-09-26 13:43:46 +0200

    sort imports

--- functions/plotting/__init__.py
+++ functions/plotting/__init__.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 from functions import *
 from functions.plotting.contour import plot_contour
+from functions.plotting.profiles import plot_alongshore, plot_crossshore
 from functions.plotting.spectrum import plot_spectrum_1d, plot_spectrum_2d
 from functions.plotting.timeseries import plot_timeseries
-from functions.plotting.profiles import plot_alongshore, plot_crossshore
 # fmt: on
