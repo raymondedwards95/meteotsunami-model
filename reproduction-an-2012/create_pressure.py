a8befa928a41a0852493ee932a662deafd67d6a1
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2022-10-13 15:15:16 +0200

    update formatting

--- reproduction-an-2012/create_pressure.py
+++ reproduction-an-2012/create_pressure.py
@@ -8,43 +8,61 @@ import dask.array as da
 import numpy as np
 import xarray as xr
 
+# fmt: off
 # fix for importing functions below
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 import functions.pressure as fp
+# fmt: on
 
 
-### Start
+# Start
 t0 = time.perf_counter()
 print(f"\nStart creating bathymetry-files for repr")
 
 
-### Parameters
+# Parameters
 # generic
 cases = [0, 10, 11, 12, 15, 16, 17, 31, 32, 33, 41]
 num_cases = len(cases)
 
 # pressure distribution
-T0 = 10000.  # default: 10000 s
-U = 50.  # default: 50 m/s
-a = 200000.  # default: 200 km
-p0 = 2000.  # default: 2000 Pa
+# defaults:
+# T0: 10000 s
+# U: 50 m/s
+# a: 200 km
+# p0: 2000 Pa
+T0 = 10000.
+U = 50.
+a = 200000.
+p0 = 2000.
 
 # cross shore (meters)
-x_min = 0.  # default: 0 km
-x_max = 1e6  # default: 1000 km
-x_steps = list(np.array([10, 5, 20, 40, 10, 10, 10, 10, 10, 10, 10]) * 1e3)  # default: 10 km, i.e. same as model-grid
+# x_min: 0 km
+# x_max: 1000 km
+# x_steps: 10 km, i.e. same as model-grid
+x_min = 0.
+x_max = 1e6
+x_steps = np.array([10, 5, 20, 40, 10, 10, 10, 10, 10, 10, 10]) * 1e3
 
 # along shore (meters)
-y_min = -1e7  # default: -10000 km
-y_max = 1e7  # default: 10000 km
-y_steps = x_steps  # default: 10 km
+# y_min: -10000 km
+# y_max: 10000 km
+# y_steps: 10 km, i.e. same as model-grid
+y_min = -1e7
+y_max = 1e7
+y_steps = x_steps
 
 # time (seconds)
-t_min = 0  # default: 0
-t_max = 55 * 3600.  # default: 70 hours
-t_steps = list(np.array([30, 30, 30, 30, 60, 20, 15, 30, 30, 30, 30]) * 60.)  # default: 30 minutes
+# defaults:
+# t_min: 0 hours
+# t_max: 70 hours
+# t_step: 30 minutes
+t_min = 0
+t_max = 55 * 3600.
+t_steps = np.array([30, 30, 30, 30, 60, 20, 15, 30, 30, 30, 30]) * 60.
 
 # other
+# x0 is the x-coordinate of the center
 x0_vals = [0.] * num_cases
 x0_vals[7] = 1e5
 x0_vals[8] = 2e5
@@ -52,14 +70,14 @@ x0_vals[9] = 3e5
 x0_vals[10] = 5e5
 
 
-### Directories
+# Directories
 current_dir = os.path.dirname(os.path.realpath(__file__))
 pressure_dir = f"{current_dir}/pressure"
 
 os.makedirs(pressure_dir, exist_ok=True)
 
 
-### Check parameters
+# Check parameters
 assert len(x_steps) == num_cases
 assert len(y_steps) == num_cases
 assert len(t_steps) == num_cases
@@ -76,7 +94,7 @@ with open(f"{pressure_dir}/parameters_pressure.txt", "w") as file:
     del line, i
 
 
-### Function
+# Function
 def pressure(x, y, t, t0=T0, U=U, a=a, p0=p0, x0=0.):
     """ Pressure disturbance distribution used for experiments
 
@@ -98,29 +116,30 @@ def pressure(x, y, t, t0=T0, U=U, a=a, p0=p0, x0=0.):
     return (
         p0
         * (1. - da.exp(-t / t0))
-        * da.exp(-((x - x0)**2. + (y - U * t)**2. ) / a**2.)
+        * da.exp(-((x - x0)**2. + (y - U * t)**2.) / a**2.)
     )
 
 
-### Compute field
+# Compute field
 for case_number in range(num_cases):
-    ## Start
+    # Start
     ta = time.perf_counter()
 
-    ## Set parameters
+    # Set parameters
     case = cases[case_number]
     x_step = x_steps[case_number]
     y_step = y_steps[case_number]
     t_step = t_steps[case_number]
     x0 = x0_vals[case_number]
 
-    print(f"\nComputing pressure field for case {case:02.0f} ({x_step:0.1f}, {y_step:0.1f}, {t_step:0.1f}, {x0:0.1f})")
+    print(
+        f"\nComputing pressure field for case {case:02.0f} ({x_step:0.1f}, {y_step:0.1f}, {t_step:0.1f}, {x0:0.1f})")
 
-    ## Set paths
+    # Set paths
     filename = f"{pressure_dir}/repr_{case:02.0f}"
     figurename = f"{pressure_dir}/fig_repr_{case:02.0f}"
 
-    ## Create grid
+    # Create grid
     x_num = int((x_max - x_min) / x_step + 1)
     y_num = int((y_max - y_min) / y_step + 1)
 
@@ -139,24 +158,24 @@ for case_number in range(num_cases):
     print(f"{xx.chunksize=}")
     print(f"{yy.chunksize=}")
 
-    ## Compute pressure
+    # Compute pressure
     print(f"Computing pressure for case {case:02.0f}")
     p = pressure(xx, yy, tt, T0, U, a, p0, x0).astype(np.float32)
 
-    ## Convert data
+    # Convert data
     print(f"Converting data for case {case:02.0f}")
     fp.convert_to_xarray(t, x, y, p, savename=filename, close=True)
     del t, x, y, p
 
-    ## Re-read data (lazy)
+    # Re-read data (lazy)
     print(f"Read data for case {case:02.0f}")
     data = xr.open_dataarray(f"{filename}.nc", chunks="auto")
 
-    ## Write pressure data
+    # Write pressure data
     print(f"Writing pressure field for case {case:02.0f}")
     fp.write_pressure(data, filename)
 
-    ## Write forcing file
+    # Write forcing file
     if case != 0:
         print(f"Overwriting forcing file for case {case:02.0f}")
         with open(f"{current_dir}/pressure/forcing_repr_{case:02.0f}.ext", "w") as file:
@@ -167,15 +186,16 @@ for case_number in range(num_cases):
             file.write("METHOD   = 1 \n")
             file.write("OPERAND  = O \n")
 
-    ## Visualise field
+    # Visualise field
     print(f"Plotting pressure field for case {case:02.0f}")
     fp.plot_pressure(data, filename=figurename, x_scales=[0, 500])
 
-    ## End
+    # End
     tb = time.perf_counter()
-    print(f"Finished creating pressure-field for {case=:02.0f} in {tb-ta:0.1f} seconds")
+    print(
+        f"Finished creating pressure-field for {case=:02.0f} in {tb-ta:0.1f} seconds")
 
 
-### End
+# End
 t1 = time.perf_counter()
 print(f"\nFinished creating pressure-files for repr in {t1-t0:0.1f} seconds")
