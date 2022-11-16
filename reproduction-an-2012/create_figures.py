91415db546034656335d5a56edb63ad724f928f8
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2022-11-16 14:42:33 +0100

    update paths

--- reproduction-an-2012/create_figures.py
+++ reproduction-an-2012/create_figures.py
@@ -35,7 +35,7 @@ case = f"{args.case:02}"
 
 # Paths
 current_dir = os.path.dirname(os.path.realpath(__file__))
-figure_dir = f"{current_dir}/figures/repr_{case}"
+figure_dir = f"{current_dir}/results/repr_{case}"
 data_dir = f"{current_dir}/output"
 
 data_file = f"{data_dir}/data_repr_{case}.nc"
