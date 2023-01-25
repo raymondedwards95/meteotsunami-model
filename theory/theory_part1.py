21e9fff6caab1831f5fd88c76a6df3f132d4dc44
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2023-01-25 14:28:13 +0100

    add bathymetry to area-plot

--- theory/theory_part1.py
+++ theory/theory_part1.py
@@ -3,10 +3,13 @@
 import os
 import sys
 
+import cmocean as cmo
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import numpy as np
 import numpy.typing as npt
+import xarray as xr
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 create_map: bool
 try:
@@ -17,6 +20,15 @@ try:
 except:
     print("Cartopy is not installed!")
     create_map = False
+
+fill_map: bool
+try:
+    import climetlab as cml
+
+    fill_map = True
+except:
+    print("Climetlab is not installed!")
+    fill_map = False
 
 # fmt: off
 # fix for importing functions below
@@ -208,9 +220,7 @@ def theory_figure_wavelength_vs_velocity(
 
 
 # Map
-def theory_figure_map(
-    savename: str = None,
-) -> None:
+def theory_figure_map(savename: str = None, bathymetry: xr.Dataset = None) -> None:
     """Create figure of the area of interest
 
     Options:
@@ -228,6 +238,24 @@ def theory_figure_map(
     fig.suptitle("Area of Interest", va="top", ha="left", x=0.01)
 
     ax = fig.add_subplot(projection=ccrs.PlateCarree())
+
+    if bathymetry is not None:
+        savename = f"{savename}-bathymetry"
+
+        cf = ax.pcolormesh(
+            data["longitude"],
+            data["latitude"],
+            data,
+            cmap=cmo.tools.crop(cmo.cm.topo, -200, 0, 0),
+            vmin=-200,
+            rasterized=True,
+        )
+
+        cb = fig.colorbar(cf, ax=ax, extend="min")
+        cb.set_label("Water Depth [m]")
+
+        ax.tick_params(labelright=False)
+
     coastlines = ax.coastlines("10m")
     landsurface = ax.add_feature(
         cfeature.NaturalEarthFeature(
@@ -235,11 +263,12 @@ def theory_figure_map(
         )
     )
     ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True)
-    ax.set_xlim([-12.5, 12.5])
+
+    ax.set_xlim([-10, 10])
     ax.set_ylim([45, 60])
     ax.set_aspect(1)
-    # ax.set_xlabel("Longitude")
-    # ax.set_ylabel("Latitude")
+    ax.set_xlabel("Longitude")
+    ax.set_ylabel("Latitude")
 
     coastlines.set(rasterized=True)
     landsurface.set(rasterized=True)
@@ -257,6 +286,26 @@ if __name__ == "__main__":
     current_dir = os.path.dirname(os.path.realpath(__file__))
     figure_dir = f"{current_dir}/figures"
 
+    # Download data if necessary
+    if fill_map:
+        source = cml.load_source(
+            "cds",
+            "reanalysis-era5-single-levels",
+            variable=["model_bathymetry"],
+            product_type="reanalysis",
+            area=[60, -15, 45, 15],
+            year="2020",
+            day="01",
+            month="01",
+            time="12:00",
+            format="netcdf",
+        )
+
+        data = source.to_xarray()
+        data = data["wmb"].sel(time=data["time"].min())
+        data = -1. * data
+        data = data.fillna(0.0)
+
     # Parameters
     a = np.logspace(2, 6, 201)
     alpha = np.sort(np.array([1 / 50, 1 / 400, 1 / 200, 1 / 800]))[::-1]
@@ -268,6 +317,8 @@ if __name__ == "__main__":
     theory_figure_wavelength_vs_velocity(velocity, alpha)
     if create_map:
         theory_figure_map()
+    if create_map and fill_map:
+        theory_figure_map(bathymetry=data)
 
     # End
     if show_figures:
