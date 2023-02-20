c733342b6c014f3e7f0c056c01b71cf79b57bac2
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2023-02-20 13:01:52 +0100

    update units

--- functions/plotting/spectrum2d.py
+++ functions/plotting/spectrum2d.py
@@ -98,9 +98,7 @@ class plot_spectrum_2d(plot_base):
 
         # General
         if self.title is None:
-            self.title = (
-                f"Power Spectrum - {self.variable_long} - $x = \SI{{{self.x / 1000:0.1f}}}{{\kilo\meter}}$"
-            )
+            self.title = f"Power Spectrum - {self.variable_long} - $x = \SI{{{self.x / 1000:0.1f}}}{{\kilo\meter}}$"
         super()._base_setup_figure()
 
     def _setup_plot(self) -> None:
@@ -205,7 +203,9 @@ class plot_spectrum_2d(plot_base):
 
         # Colorbar
         self.cbar = self.fig.colorbar(self.im, cax=self.cax)
-        self.cbar.set_label("Spectral power")  #TODO COMPUTE UNITS
+        self.cbar.set_label(
+            f"Spectral Power [\si{{\square\meter {self.unit} \hour}}]"
+        )  # TODO CHECK UNITS
 
         # Set plot limits
         self.k_max_scaled = np.min(
