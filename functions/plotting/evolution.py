204857e03c17f048eb8e0eaeb7b352807313fe43
Raymond Edwards <raymondedwards95@users.noreply.github.com>
2023-05-09 13:45:43 +0200

    test new visualisation

--- functions/plotting/evolution.py
+++ functions/plotting/evolution.py
@@ -16,35 +16,55 @@ script_dir = f"{PATH_PLOTTING}"
 figure_dir = f"{PATH_TEST}"
 
 data_a = f"{PATH_MAIN}/reproduction-an-2012/output/data_repr_00.nc"
-data_b = f"{PATH_MAIN}/reproduction-an-2012/output/data_repr_01.nc"
+data_b = f"{PATH_MAIN}/reproduction-an-2012/output/data_repr_17.nc"
 
 data_a = xr.open_dataset(data_a, chunks="auto")
 data_b = xr.open_dataset(data_b, chunks="auto")
 
+x_ref = 1e4
 
-plt.figure()
-plt.plot(
-    data_a["t"].values.astype("datetime64[s]").astype(float) / 3600.0,
-    data_a["wl"].max(dim=["x", "y"]),
-    label="00 max",
-)
-plt.plot(
-    data_a["t"].values.astype("datetime64[s]").astype(float) / 3600.0,
-    -1.0 * data_a["wl"].min(dim=["x", "y"]),
-    label="00 min",
-)
-plt.plot(
-    data_b["t"].values.astype("datetime64[s]").astype(float) / 3600.0,
-    data_b["wl"].max(dim=["x", "y"]),
-    label="01 max",
-)
-plt.plot(
-    data_b["t"].values.astype("datetime64[s]").astype(float) / 3600.0,
-    -1.0 * data_b["wl"].min(dim=["x", "y"]),
-    label="01 min",
-)
-plt.ylim(0, None)
-plt.legend()
-plt.xlabel("Time [hours]")
-plt.ylabel("Water Level [meters]")
-plt.savefig(f"{figure_dir}/test_evolution")
+
+fig, axes = plt.subplots(2, 1, sharex=True)
+
+for i, data in enumerate([data_a, data_b]):
+    for j, var in enumerate(["wl", "p"]):
+        data_time = data["t"].values.astype("datetime64[s]").astype(float) / 3600.0
+        data_max = data[var].interp(x=x_ref).max(dim=["y"])
+        data_min = -1.0 * data[var].interp(x=x_ref).min(dim=["y"])
+
+        axes[j].plot(
+            data_time,
+            data_max,
+            label=f"{i} max",
+        )
+        axes[j].fill_between(
+            data_time,
+            data_max,
+            alpha=0.02,
+        )
+
+        axes[j].plot(
+            data_time,
+            data_min,
+            label=f"{i} min",
+        )
+        axes[j].fill_between(
+            data_time,
+            data_min,
+            alpha=0.02,
+        )
+
+for ax in axes:
+    ax.set_ylim(0, None)
+    ax.legend()
+    ax.grid()
+
+axes[1].set_xlabel("Time [hours]")
+axes[0].set_ylabel("Water Level \n[meters]")
+axes[1].set_ylabel("Surface Air Pressure \n[pascals]")
+
+fig.align_labels()
+
+plt.savefig(f"{figure_dir}/test_evolution",
+        bbox_inches="tight",
+        pil_kwargs=FIG_PIL_KWARGS,)
